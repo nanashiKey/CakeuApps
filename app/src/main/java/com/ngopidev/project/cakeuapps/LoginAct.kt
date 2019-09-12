88ce7a0e458ea26d7226741dc58858a0cdc4d93a
Irfan <assidiq.irfan@gmail.com>
2019-09-12 10:50:44 +0700

    yes add some activity

---
+++ app/src/main/java/com/ngopidev/project/cakeuapps/LoginAct.kt
@@ -0,0 +1,12 @@
+package com.ngopidev.project.cakeuapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
